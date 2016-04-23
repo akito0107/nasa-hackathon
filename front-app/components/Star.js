6b89fa337da8bdcb2648c32a3f3507b3cf5e0c22
yuzutas0 <sho.y0921@gmail.com>
2016-04-24 04:03:54 +0900

    add star-modal-jsx

--- front-app/components/Star.js
+++ front-app/components/Star.js
@@ -7,5 +7,20 @@ export default class Star extends React.Component {
         //   return(<Modal name={modal.name}>{modal.color}</Modal>);
         // });
         // return(<div className='star'>{modalNodes}</div>);
+        return(
+            <div className="modal star-modal modal-blue">
+                <div className="modal-header">
+                    <div className="modal-title modal-title-blue modal-title-left"><p>ベテルギウス - HIP: 27989</p></div>
+                    <div className="modal-close"><p>×</p></div>
+                </div>
+                <div className="modal-body">
+                    <div><p>Team: <span className="modal-contents-blue">Earth</span></p></div>
+                    <div><p>Score: <span className="modal-contents-blue">10</span></p></div>
+                    <div><p>オリオン座 を構成する星の1つです</p></div>
+                    <hr className="modal-hr modal-hr-blue" />
+                    <div className="modal-button modal-button-blue"><p>Hack</p></div>
+                </div>
+            </div>
+        )
     }
 }
