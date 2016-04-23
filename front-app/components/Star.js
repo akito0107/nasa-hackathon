0ab53026597353ae448729bb0b9a023d906702c8
yuzutas0 <sho.y0921@gmail.com>
2016-04-24 03:56:26 +0900

    comment out

--- front-app/components/Star.js
+++ front-app/components/Star.js
@@ -3,9 +3,9 @@ import Modal from './Modal'
 
 export default class Star extends React.Component {
   render() {
-    var modalNodes = this.props.data.map((modal)=> {
-      return(<Modal name={modal.name}>{modal.color}</Modal>);
-    });
-    return(<div className='star'>{modalNodes}</div>);
+    // var modalNodes = this.props.data.map((modal)=> {
+    //   return(<Modal name={modal.name}>{modal.color}</Modal>);
+    // });
+    // return(<div className='star'>{modalNodes}</div>);
   }
 }
